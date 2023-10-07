dcfba5bff8cd14a5bb18274cc0890c67dccd7c4d
Marshall Thompson <marshall@creativeideal.net>
2023-10-06 23:02:13 -0600

    rewrite badges adapter to accept package-name

--- docs/.vitepress/components/Badges.vue
+++ docs/.vitepress/components/Badges.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 // A style wrapper to make npm badges do flex-row.
+const props = defineProps<{
+  packageName?: string
+}>()
+
+const shortName = computed(() => {
+  if (!props.packageName) return ''
+  const parts = props.packageName.split('/')
+  return parts[parts.length - 1]
+})
 </script>
 
 <template>
-  <div class="badges"><slot /></div>
+  <div class="flex flex-row items-center gap-1 pt-2">
+    <a :href="`https://www.npmjs.com/package/${packageName}`">
+      <img :src="`https://img.shields.io/npm/v/${packageName}.svg?style=flat-square`" alt="npm version">
+    </a>
+    <a :href="`https://github.com/wingshq/wings/blob/main/packages/${shortName}/CHANGELOG.md`">
+      <img :src="`https://img.shields.io/badge/changelog-.md-blue.svg?style=flat-square`" alt="Changelog">
+    </a>
+  </div>
 </template>
 
 <style lang="postcss">
@@ -11,6 +28,7 @@
 .badges p {
   @apply flex flex-row;
 }
+
 .badges img {
   margin-right: .5em
 }
